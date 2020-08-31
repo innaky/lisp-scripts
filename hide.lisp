fa108beeb3549f8e0e4323eb127260281555d45b
Innaky <innaky@protonmail.com>
2020-08-31 02:33:53 -0400

    Hide stable Delete copy-file, mv and some others functions, replaced by rename-file function transformation-file, apply the logic of build the from-to paths and rename the directory to destiny path hide stable version of hide function.

--- hide.lisp
+++ hide.lisp
@@ -10,34 +10,21 @@
   (aref lst (random (length lst))))
 
 (defun random-string (&optional (length 15))
+  "Build a random string, the default is a string with 15 letters"
   (if (equal length 0)
       nil
       (cons (random-elem *characters*)
 	    (random-string (- length 1)))))
 
-(defun copy-file (from to)
-  "Copy a file `from' a path to another path `to'"
-  (with-open-file (input-stream from)
-    (with-open-file (output-stream to
-				   :direction :output
-				   :if-does-not-exist :create
-				   :if-exists :supersede)
-      (when input-stream
-	(loop for line = (read-line input-stream nil)
-	     while line do (format output-stream "~A~%" line))))))
-
-(defun mv (from-path to-path)
-  "Copy a file in `to-path' and delete of `from-path'"
-  (copy-file from-path to-path)
-  (delete-file from-path))
-
 (defun concatenate-elems-of-lst (lst-of-strings)
+  "Receive a list of strings and return the words concatenated in a string."
   (if (equal nil lst-of-strings)
       nil
       (concatenate 'string (car lst-of-strings)
 		   (concatenate-elems-of-lst (cdr lst-of-strings)))))
 
 (defun base-path (one-file-path)
+  "Build the base-path of the `one-file-path'."
   (reverse
    (cdr
     (reverse
@@ -45,37 +32,22 @@
 		      (concatenate 'string "/" elem))
 		  (cl-ppcre:split "/" one-file-path)))))))
 
-(defun transformation-not-mv (one-file-path)
+(defun transformation-file (one-file-path)
+  "This function return a list with the absolute path of `one-file-path' and with the absolute path
+of the new name, and rename the `one-file-path' to the new name."
   (let ((random-name (concatenate 'string (concatenate-elems-of-lst (base-path one-file-path))
 				  "/" (random-string))))
     (cons one-file-path
-	  (cons (concatenate 'string random-name) nil))))
-
-(defun transformation (one-file-path)
-  (let ((random-name (concatenate 'string "/" (random-string)))
-	(base-file (concatenate-elems-of-lst (base-path one-file-path))))
-    (progn
-      (cons one-file-path
-	    (cons (concatenate 'string random-name) nil))
-      (mv one-file-path base-file))))
-
-(defun hide-not-mv (db-file directory-to-hide)
-  (with-open-file (stream db-file
-			  :direction :output
-			  :if-does-not-exist :create
-			  :if-exists :supersede)
-    (format stream "~A~%" (mapcar #'(lambda (one-file-path)
-				      (transformation-not-mv (namestring one-file-path)))
-				  (cl-fad:list-directory directory-to-hide)))))
+	  (cons random-name nil))
+    (rename-file one-file-path random-name)))
 
 (defun hide (db-file directory-to-hide)
+  "Save a `db-file' in a path and change the name of the directory `directory-to-hide'."
   (with-open-file (stream db-file
 			  :direction :output
 			  :if-does-not-exist :create
 			  :if-exists :supersede)
     (format stream "~A~%" (mapcar #'(lambda (one-file-path)
-				      (let ((random-name (random-string)))
-					(cons one-file-path
-					      (cons (concatenate 'string random-name) nil))))
+				      (transformation-file (namestring one-file-path)))
 				  (cl-fad:list-directory directory-to-hide)))))
 
